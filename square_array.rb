b6b0a31f1defe2612e4cb85908d316b146196434
Darrian Soto <ddog350@gmail.com>
2019-09-24 05:59:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_array = []
+  numbers = [1,2,3]
+  numbers.each do |x|
+    x = x * x
+    new_array.push(x)
+  end
+  
 end
